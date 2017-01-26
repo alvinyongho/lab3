264b893c523034865ed000ac4359df66b3f36743
alvinyho <alvinyongho@gmail.com>
2017-01-26 09:44:03 -0800

    completion of assignment

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -10,9 +10,58 @@ $(document).ready(function() {
  */
 function initializePage() {
 	$("#testjs").click(function(e) {
-		$('.jumbotron h1').text("Javascript is connected");
+		$('.jumbotron h1').text(selectRandStr());
+
+		$("#testjs").text("Please wait...");
+		$(".jumbotron p").addClass("active");
+
+
 	});
+
 
 	// Add any additional listeners here
 	// example: $("#div-id").click(functionToCall);
+
+	$("a").click(projectClick);
+}
+
+
+function selectRandStr(){
+	console.log("test");
+	var textArr = [
+		"Experience Chan! It's not a lot of questions.",
+		'Too many questions is the Chan disease.',
+		'The best way is just to observe the noise of the world.',
+		'The answer to your questions?',
+		'Ask your own heart.'
+	]
+	var randomNumber = Math.floor(Math.random()*textArr.length);
+	return textArr[randomNumber];
+}
+
+
+
+
+function projectClick(e){
+	e.preventDefault();
+
+	var containingProject = $(this).closest(".project");
+    var description = $(containingProject).find(".project-description");
+    if (description.length == 0) {
+			//  $(".project-description").show()
+			 $(containingProject).append("<div class='project-description'><p>Description of the project.</p></div>");
+    } else {
+      //  description.html("<p>Stop clicking on me! You just did it at " + (new Date()) + "</p>");
+			 $(".project-description").fadeOut()
+			 description.html("");
+		}
+
+		console.log($(".project-description").is(":visible"));
+		var isVisible = $(".project-description").is(":visible")
+		if (!isVisible){
+			$(".project-description").show()
+			$(".project-description").html("<div class='project-description'><p>Description of the project.</p></div>")
+
+		}
+
 }
